96c0b969b7c76f7c06602fe7aea479f00fbedcb4
krivonosovti <111201189+krivonosovti@users.noreply.github.com>
2024-01-27 14:38:26 +0300

    k

--- src/main/java/medika/it/equipment_manager/entity/ValueEntity.java
+++ src/main/java/medika/it/equipment_manager/entity/ValueEntity.java
@@ -1,4 +1,33 @@
 package medika.it.equipment_manager.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.function.LongFunction;
+
+@Entity
 public class ValueEntity {
+    @Id
+    private Long Id;
+
+    private String Value;
+
+    public ValueEntity() {
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getValue() {
+        return Value;
+    }
+
+    public void setValue(String value) {
+        Value = value;
+    }
 }
